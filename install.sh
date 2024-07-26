03edede706cf92bec04f7ffe28613774fbfeb9c4
gpigna0 <gabrypigna2@gmail.com>
2024-07-26 23:55:22 +0200

    [MOD] Lo user di Redis viene creato anche con i permessi per DELETE e KEYS

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ fi
 echo "Se non è già presente creo uno user per accedere al database"
 if [ -z "$(redis-cli acl getuser ITDSBOT)" ] # Non è presente lo user ITDSBOT
 then
-  redis-cli acl setuser ITDSBOT \>itds on \~itds:\* +set +get +ping
+  redis-cli acl setuser ITDSBOT \>itds on \~itds:\* +set +get +del +keys +ping
 fi
 if [ -s config.py ]
 then
--- rundiscord.sh
+++ rundiscord.sh
@@ -1,6 +1,6 @@
 if [ -z "$(redis-cli acl getuser ITDSBOT)" ] # Non è presente lo user ITDSBOT
 then
-  redis-cli acl setuser ITDSBOT \>itds on \~itds:\* +set +get +ping
+  redis-cli acl setuser ITDSBOT \>itds on \~itds:\* +set +get +del +keys +ping
 fi
 . ./venv/bin/activate
 ./itdsdiscordbot.py
